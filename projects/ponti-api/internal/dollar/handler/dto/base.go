8d926ff25368bc03b57e0a24c4b4a179241f6070
luka385 <lucasbrandan26@gmail.com>
2025-07-15 11:56:28 +0000

    better dto

--- projects/ponti-api/internal/dollar/handler/dto/base.go
+++ projects/ponti-api/internal/dollar/handler/dto/base.go
@@ -1,14 +1,18 @@
 package dto
 
 import (
+	"time"
+
 	"github.com/alphacodinggroup/ponti-backend/projects/ponti-api/internal/dollar/usecases/domain"
 )
 
 type MonthResponse struct {
-	Month        string  `json:"month"`
-	StartValue   float64 `json:"start_value"`
-	EndValue     float64 `json:"end_value"`
-	AverageValue float64 `json:"average_value"`
+	Month        string    `json:"month"`
+	StartValue   float64   `json:"start_value"`
+	EndValue     float64   `json:"end_value"`
+	AverageValue float64   `json:"average_value"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 func FromDomainMonth(d *domain.DollarAverage) MonthResponse {
@@ -17,5 +21,7 @@ func FromDomainMonth(d *domain.DollarAverage) MonthResponse {
 		StartValue:   d.StartValue,
 		EndValue:     d.EndValue,
 		AverageValue: d.AvgValue,
+		CreatedAt:    d.CreatedAt,
+		UpdatedAt:    d.UpdatedAt,
 	}
 }
