2a9e4a126edd8d5ec0c00cbf6f2f2170edbd91ae
maumiranda_meli <mauro.miranda@mercadolibre.com>
2025-07-19 15:36:46 -0300

    migrations

--- projects/ponti-api/migrations/000001_init_schema.up.sql
+++ projects/ponti-api/migrations/000001_init_schema.up.sql
@@ -9,21 +9,21 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
-                       email TEXT NOT NULL,
-                       username TEXT UNIQUE NOT NULL,
-                       password TEXT NOT NULL,
-                       token_hash TEXT NOT NULL,
-                       refresh_tokens TEXT[] DEFAULT ARRAY[]::TEXT[],
-                       id_rol INT NOT NULL,
-                       is_verified BOOLEAN DEFAULT FALSE,
-                       active BOOLEAN DEFAULT TRUE,
-                       created_by INT NOT NULL,
-                       updated_by INT NOT NULL,
-                       deleted_by INT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       deleted_at TIMESTAMP NULL
+    id BIGSERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    token_hash TEXT NOT NULL,
+    refresh_tokens TEXT[] DEFAULT ARRAY[]::TEXT[],
+    id_rol INT NOT NULL,
+    is_verified BOOLEAN DEFAULT FALSE,
+    active BOOLEAN DEFAULT TRUE,
+    created_by INT NOT NULL,
+    updated_by INT NOT NULL,
+    deleted_by INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TRIGGER set_timestamp
@@ -220,5 +220,21 @@ CREATE TABLE labor_categories (
     updated_by VARCHAR(255),
     deleted_by VARCHAR(255)
 );
+
+CREATE TABLE labors (
+    id BIGSERIAL PRIMARY KEY,
+    project_id INTEGER NOT NULL REFERENCES projects(id),
+    name TEXT NOT NULL,
+    category_id INTEGER NOT NULL REFERENCES labor_categories(id),
+    price NUMERIC(10,2) NOT NULL,
+    contractor_name TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    deleted_at TIMESTAMPTZ,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    deleted_by VARCHAR(255)
+);
+
 
 
