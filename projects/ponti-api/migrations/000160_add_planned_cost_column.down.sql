8736b83b260df37ad385facf3c72af4abb485b64
ext-pacristo_meli <ext_pacristo@mercadolibre.com>
2025-10-21 16:51:21 -0300

    env

--- projects/ponti-api/migrations/000160_add_planned_cost_column.down.sql
+++ projects/ponti-api/migrations/000160_add_planned_cost_column.down.sql
@@ -1,2 +1,12 @@
-ALTER TABLE projects
-DROP COLUMN planned_cost;
+-- Eliminar columna planned_cost solo si existe (idempotente)
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name = 'projects' 
+        AND column_name = 'planned_cost'
+    ) THEN
+        ALTER TABLE projects DROP COLUMN planned_cost;
+    END IF;
+END $$;
